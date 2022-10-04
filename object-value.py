03f37f88f7d3e6ef9c5e9bcf89370eb5aaef1adc
shaik212 <80620740+shaik212@users.noreply.github.com>
2022-10-04 18:05:47 +0530

    Update object-value.py

--- object-value.py
+++ object-value.py
@@ -1,8 +1,18 @@
- #!/bin/bash
-function get_value()
-{
-  key=$(echo $2 | tr '/' '.')
-  echo '$1' > obj.json
-  jq -r .$key obj.json
-}
-get_value $1 $2
+let str = `{"return":
+                     {
+                       "response": [{"$": 1234}],
+                       "responseMessage": [{"$": "Success ABC"}],
+                       "responseCode": [{"$": "CITY,India"}]
+                     }
+                    }`;
+    
+    
+        let js_object = JSON.parse(str); // parse json string to javascript object
+        let js_object_return = js_object.return;
+        let formated_obj = {};
+        let desired_obj = {};
+        Object.keys(js_object_return).forEach(function(key) {
+            formated_obj[key] = js_object_return[key][0]["$"];
+        });
+        desired_obj['return']=formated_obj;
+        console.log(desired_obj.return.response);
