f9c82f397796d612f168d58dbf477710b57981da
shaik212 <80620740+shaik212@users.noreply.github.com>
2022-10-04 18:00:49 +0530

    Update object-value.py

--- object-value.py
+++ object-value.py
@@ -1,22 +1,8 @@
-from functools import reduce
-
-#Take the key input from user and stores it in a variable k
-k = input("Enter the keys: ")
-
-#Function is defined to take object and keys as input
-def deep_get(dictionary,keys, default=None ):
-
-    #Reduce and lambda to check the key value and return the output
-    a = reduce(lambda z, key: z.get(key, default) if isinstance(z, dict) else default, keys.split("/"), dictionary)
-
-    #If Condition to make sure there is no output in partial correct key is passed
-    if isinstance(a, str):
-        return a
-    else:
-        return None
-
-
-object = {'a':{'b':{'c':'d'}}}
-
-#Call the function and pass the values in it, print the return output
-print (deep_get(object, k))
+ #!/bin/bash
+function get_value()
+{
+  key=$(echo $2 | tr '/' '.')
+  echo '$1' > obj.json
+  jq -r .$key obj.json
+}
+get_value $1 $2
